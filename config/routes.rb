36ac6ff9a1186b1fe1b99bc60beb87b478849b17
Daniel Maile-Wilson <danny.mw1991@gmail.com>
2021-11-16 17:50:44 +0000

    updated the routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
     resources :bookings
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/mypowers', to: 'powers#my_powers'
+  get '/my_powers', to: 'powers#my_powers'
 
 end
