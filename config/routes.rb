7688f3eb360115e54926d8544d909f84bc4ea1fb
Seulkee Kang <seulkeekang@gmail.com>
2021-11-16 17:46:52 +0000

    added method and route for show in bookings controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
 
   root to: 'powers#index'
   resources :powers do
-    resources :bookings, except: [:index]
+    resources :bookings, except: [:index, :show]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/mypowers', to: 'powers#my_powers'
   get '/my_bookings', to: 'bookings#index'
+  get '/my_bookings/:id', to: 'bookings#show'
 end
