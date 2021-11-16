73efdefc3fc1908aeead8b1c4286e28d35f47829
Daniel Maile-Wilson <danny.mw1991@gmail.com>
2021-11-16 11:20:33 +0000

    power type

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,7 @@ User.destroy_all
 
 user1 = User.create(email: 'dan@gmail.com', password: '123123', first_name: 'Dan', last_name: 'Danson')
 
-Power.create(user_id: user1.id, element: "fire", power_type: 'sorcery', price: 150, location: 'france', example_hero: 'human torch', name: 'pyromancer' )
+
+5.times do
+  Power.create(user_id: user1.id, element: "fire", power_type: 'sorcery', price: 150, location: 'france', example_hero: 'human torch', name: 'pyromancer' )
+end
