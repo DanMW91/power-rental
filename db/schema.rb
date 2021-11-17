49aacf07e76c3045742d8d5d9c52511fde55d383
Daniel Maile-Wilson <danny.mw1991@gmail.com>
2021-11-17 13:35:15 +0000

    user can add image url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_17_120054) do
+ActiveRecord::Schema.define(version: 2021_11_17_131825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_11_17_120054) do
     t.string "name"
     t.string "description"
     t.text "keywords"
+    t.string "image_url"
     t.index ["user_id"], name: "index_powers_on_user_id"
   end
 
